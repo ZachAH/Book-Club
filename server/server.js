20718283311ecca91a09294ad71fb9dc0498ccdd
ZMAN <zachh@github.com>
2021-02-10 11:24:22 -0600

    db

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 //import apollo
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
+
 
 
 //import typeDefs and resolvers
